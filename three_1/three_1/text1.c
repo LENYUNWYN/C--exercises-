f9173b2a6a41cba70ae333bd31870344c947d929
LENYUNwyn <1028641030@qq.com>
2021-03-20 21:53:55 +0800

    OK

---
+++ three_1/three_1/text1.c
@@ -0,0 +1,40 @@
+//华氏温pri度转化为摄氏温度
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	double  c, f,i;
+//	printf("输入f:");
+//	scanf("%lf", &f);
+//	i = 5.0 / 9;
+//	c = i*(f - 32);
+//	printf("\n");
+//	printf("c=%lf", c);
+//	getchar();
+//	return 0;
+//}
+
+//计算存款利息
+//int main()
+//{
+//	double r1 = 0.0036, r2 = 0.0225, r3 = 0.0198, p0 = 1000, p1, p2, p3;
+//	p1 = p0*(1 + r1);
+//	p2 = p0*(1 + r2);
+//	p3 = p0*(1 + r3/2)*(1 + r3 / 2);
+//	printf("%lf\n%lf\n%lf", p1, p2, p3);
+//	getchar();
+//	return 0;
+//}
+
+//给定一个大写字母，求小写字母
+//int main()
+//{
+//	char c1, c2;
+//	c1 = 'A';
+//	c2 = 32 + c1;
+//	printf("%c\n", c2);
+//	return 0;
+//}
+
+
+
