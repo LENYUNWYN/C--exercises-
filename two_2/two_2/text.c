beeda371c771c17814c7f8c97d7bbff11838cc4c
LENYUNwyn <1028641030@qq.com>
2021-03-26 23:02:34 +0800

    坚持

---
+++ two_2/two_2/text.c
@@ -0,0 +1,129 @@
+//根据月份判断，春夏秋冬
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int month;
+//	printf("输入月份：");
+//	scanf("%d", &month);
+//	if (3 == month || 4 == month || 5 == month)
+//	{
+//		printf("春季\n");
+//	}
+//	else if (6 == month || 7 == month || 8 == month)
+//	{
+//		printf("夏季\n");
+//	}
+//	else  if (9 == month || 10 == month || 11 == month)
+//	{
+//		printf("秋季\n");
+//	}
+//	else if (1 == month || 2 == month || 12 == month)
+//	{
+//		printf("冬季\n");
+//	}
+//	else { printf("输入错误！"); }
+//	getchar();
+//	return 0;
+//
+//}
+//switch写法
+//int main()
+//{
+//	int month;
+//	printf("输入月份：");
+//	scanf("%d",&month);
+//	switch (month)
+//	{
+////	default:printf("输入错误\n"); break;正确
+//	case 1:
+//	case 2:
+////	default:printf("输入错误\n"); break;错误
+//	case 12:
+//		printf("冬季\n"); break;
+////	default:printf("输入错误\n"); break;正确
+//	case 3:
+//	case 4:
+//	case 5:
+//		printf("春季\n"); break;
+//	case 6:
+//	case 7:
+//	case 8:
+//		printf("夏季\n"); break;
+//	case 9:
+//	case 10:
+//	case 11:
+//		printf("秋季\n"); break;
+//	//default:printf("输入错误\n");正确
+//
+//	}
+//	return 0;
+//}
+
+//a=10,b=20,最大
+//int main()
+//{
+//	int a = 10, b = 20;
+//	if (a > b)
+//	{
+//		printf("%d\n", a);
+//	}
+//	else {
+//		printf("%d\n", b);
+//	}
+//	return 0;
+//}
+
+//已知成绩85分，请用if else 判断小明那个等级
+//int main()
+//{
+//	float score=95.7f;
+//	if (score < 60)
+//	{
+//		printf("不及格\n");
+//	}
+//	else if (score >= 60 && score <= 70) {
+//		printf("及格\n");
+//	}
+//	else if (score>70 && score <= 90) {
+//		printf("良好\n");
+//	}
+//	else if (score > 90 && score <= 100) {
+//		printf("优秀\n");
+//	}
+//	else printf("输入错误\n");
+//	printf("加油，继续努力！\n");
+//	return 0;
+//}
+//	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
